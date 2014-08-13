c98832279a45e7e271df861d9f44da656ca5e52c
Ramy Asselin <ramy.asselin@hp.com>
2014-08-13 22:16:57 +0000

    Move scripts to jenkins jobs

--- passthrough_scripts/scripts_on_instance/find_ip.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-output = sys.stdin.read()
-
-lines = output.split("\n")
-
-for i in range(len(lines)):
-    if "eth0" in lines[i]:
-        print lines[i+1].strip().split("inet addr:")[1].split(" ")[0]
